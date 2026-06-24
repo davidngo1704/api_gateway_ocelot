6fe2a87728b989ba5907448dd91605085db6037f
Ngô Thành Đại <davidngo1704@gmail.com>
2026-06-25 02:02:38 +0700

    ok

---
+++ ApiGateway/Controllers/All/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Controllers.All
+{
+    [ApiController]
+    [Route("api/all")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllData()
+        {
+            // Implementation for retrieving all data
+            return Ok(new { message = "All data endpoint" });
+        }
+    }
+}
